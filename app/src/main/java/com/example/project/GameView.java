c43e36f864463ee425fe2c147a9eb7117efb0af3
Junbeomb <junbeomb216@gmail.com>
2024-04-15 00:38:14 +0900

    First Commit

---
+++ app/src/main/java/com/example/project/GameView.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.Choreographer;
+import android.view.Choreographer;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class GameView extends View{
+    public GameView(Context context){
+        super(context);
+    }
+}
