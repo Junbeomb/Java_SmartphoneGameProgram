dfe1e40ee6d2ac5d230a4bf08750d45855f76086
Park Jun Beom <junbeomb216@gmail.com>
2024-05-15 23:13:57 +0900

    Fixed gitignore

--- app/src/main/java/com/example/project/framework/objects/RightButton.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.framework.objects;
-
-public class RightButton extends InteractButton{
-    public RightButton(int bgBmpId, float X, float Y){
-        super(bgBmpId,X,Y);
-    }
-}
