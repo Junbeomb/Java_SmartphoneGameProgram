92f0eaa7612beb5236386058068b3ec1b0f2cdd7
Park Jun Beom <junbeomb216@gmail.com>
2024-06-08 14:44:41 +0900

    Add Trap1

---
+++ app/src/main/java/com/example/project/CatchMonster/game/BossBombSkill.java
@@ -0,0 +1,67 @@
+package com.example.project.CatchMonster.game;
+
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import com.example.project.CatchMonster.R;
+import com.example.project.framework.interfaces.IBoxCollidable;
+import com.example.project.framework.objects.SheetSprite;
+import com.example.project.framework.scene.Scene;
+
+public class BossLigtningSkill extends SheetSprite implements IBoxCollidable {
+
+    private boolean noDamage;
+    private RectF collisionRect = new RectF();
+    protected Rect[][] srcRectsArray = {
+            makeRects(300, 301, 302, 303, 304, 305, 306, 307), // State.running
+    };
+
+    private void fixCollisionRect() {
+        collisionRect.set(
+                dstRect.left + 0.6f,
+                dstRect.top,
+                dstRect.right - 0.6f,
+                dstRect.bottom);
+    }
+    protected Rect[] makeRects(int... indices) {
+        Rect[] rects = new Rect[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            int idx = indices[i];
+            int l = (idx % 300) * 200;
+            int t = 0;
+            rects[i] = new Rect(l, t, l + 200, t + 300);
+        }
+        return rects;
+    }
+    public BossLigtningSkill(float x, float y) {
+        super(R.mipmap.catchmonster_lightning, 4);
+        setPosition(x, 4.0f, 1.5f, 11.f);
+        fixCollisionRect();
+        noDamage = true;
+        srcRects = makeRects(300, 301, 302, 303, 304, 305, 306, 307, 308);
+    }
+
+    private float seconds = 0.f;
+    @Override
+    public void update(float elapsedSeconds) {
+        seconds = seconds + elapsedSeconds;
+
+        //1초 지난뒤에 데미지 주기
+        if(noDamage && seconds >=1.0f){
+            noDamage = false;
+        }
+
+        if(seconds >= 2.0f){
+            Scene.top().remove(MainScene.Layer.bossLighting, this);
+        }
+    }
+
+    @Override
+    public RectF getCollisionRect() {
+        if(noDamage)
+            return new RectF(0,0,0,0);
+        else
+            return collisionRect;
+    }
+
+}
---
+++ app/src/main/java/com/example/project/CatchMonster/game/Trap1.java
@@ -0,0 +1,115 @@
+package com.example.project.CatchMonster.game;
+
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import com.example.project.CatchMonster.R;
+import com.example.project.framework.interfaces.IBoxCollidable;
+import com.example.project.framework.objects.SheetSprite;
+import com.example.project.framework.scene.Scene;
+
+public class Trap1 extends SheetSprite implements IBoxCollidable {
+
+    public float seconds = 0.f;
+    private boolean drop = false;
+    private boolean pang = false;
+    protected Rect[][] srcRectsArray = {
+            makeRects(0,1,2,3,4,5,6,7,8,9,10,11),
+            makeRects(0,1,2,3,4,5,6,7,8)
+    };
+
+    private RectF collisionRect = new RectF();
+    private void fixCollisionRect() {
+        collisionRect.set(
+                dstRect.left + 0.2f,
+                dstRect.top,
+                dstRect.right - 0.2f,
+                dstRect.bottom);
+    }
+
+    protected Rect[] makeRects(int... indices) {
+        Rect[] rects = new Rect[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            int idx = indices[i];
+            int l = idx * 50;
+            int t = 0;
+
+            rects[i] = new Rect(l, t, l + 50, t + 50);
+        }
+        return rects;
+    }
+
+    protected Rect[] makeRects2() {
+        Rect[] rects = new Rect[1];
+        rects[0] = new Rect(0, 50, 50, 100);
+        return rects;
+    }
+
+    protected Rect[] makeRects3(int... indices) {
+        Rect[] rects = new Rect[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            int idx = indices[i];
+            int l = idx * 100;
+            int t = 100;
+
+            rects[i] = new Rect(l, t, l + 100, t + 50);
+        }
+        return rects;
+    }
+    public Trap1(float x, float y) {
+        super(R.mipmap.catchmonster_trap1, 5);
+        setPosition(x, y, 1.f, 1.f);
+        srcRects = makeRects(0,1,2,3,4, 5,6,7,8,9, 10,11);
+        seconds = 0.f;
+    }
+
+    private float yy;
+    private boolean pangEarly = false;
+    @Override
+    public void update(float elapsedSeconds) {
+        seconds += elapsedSeconds;
+        if(!drop && seconds >= 2.4){
+            drop = true;
+        }
+        if(drop){
+            yy = y+ seconds*0.03f;
+            srcRects = makeRects2();
+            setPosition(x, yy, 1.f, 1.f);
+
+            if(y+seconds > 11.f){
+                drop = false;
+                pang = true;
+                seconds = 0.f;
+            }
+        }
+        if(pang){
+            if(seconds > 1.5f){
+                Scene.top().remove(MainScene.Layer.trap1, this);
+            }
+            setPosition(x, 7.f, 1.f, 1.f);
+            srcRects = makeRects3(0,1,2,3,4, 5,6,7,8);
+        }
+        if(pangEarly){
+            if(seconds > 1.5f){
+                Scene.top().remove(MainScene.Layer.trap1, this);
+            }
+        }
+        fixCollisionRect();
+    }
+
+    public void pangFunc(float currentY){
+        drop = false;
+        pang = false;
+        pangEarly = true;
+        seconds = 0.f;
+        setPosition(x, currentY, 1.f, 1.f);
+        srcRects = makeRects3(0,1,2,3,4, 5,6,7,8);
+    }
+    @Override
+    public RectF getCollisionRect() {
+        if(pangEarly || pang)return new RectF(0,0,0,0);
+
+        return collisionRect;
+    }
+
+}
