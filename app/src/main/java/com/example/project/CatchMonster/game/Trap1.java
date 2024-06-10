02503beb2898f94b6a5522974637048ab4f2b8c7
Park Jun Beom <junbeomb216@gmail.com>
2024-06-10 16:41:52 +0900

    Fixed trap bugs

--- app/src/main/java/com/example/project/CatchMonster/game/Trap1.java
+++ app/src/main/java/com/example/project/CatchMonster/game/Trap1.java
@@ -10,10 +10,14 @@ import com.example.project.framework.scene.Scene;
 
 public class Trap1 extends SheetSprite implements IBoxCollidable {
 
+    public enum State {
+        idle, ready,drop,pang, earlypang
+    }
+    protected Trap1.State state = Trap1.State.idle;
+    private void setState(Trap1.State state) {
+        this.state = state;
+    }
     public float seconds;
-    private boolean drop = false;
-    private boolean ready = false;
-    private boolean pang = false;
     protected Rect[][] srcRectsArray = {
             makeRects(0,1,2,3,4,5,6,7,8,9,10,11),
             makeRects(0,1,2,3,4,5,6,7,8)
@@ -62,68 +66,70 @@ public class Trap1 extends SheetSprite implements IBoxCollidable {
     public Trap1(float x, float y) {
         super(R.mipmap.catchmonster_trap1, 5);
         setPosition(x, y, 1.f, 1.f);
-
+        this.fps =5;
         startTime = (float)Math.random();
-        startTime*=10;
-        ready = false;
+        startTime *=10;
+
+        setState(State.idle);
 
         seconds = 0.f;
         srcRects = makeRects(0);
     }
 
     private float yy;
-    private boolean pangEarly = false;
     @Override
     public void update(float elapsedSeconds) {
 
         seconds += elapsedSeconds;
-
-        if(!ready && seconds >= startTime){
-            ready = true;
-            srcRects = makeRects(0,1,2,3,4, 5,6,7,8,9, 10,11);
-            seconds = 0.f;
-        }
-
-        if(ready && !drop && seconds >= 2.0){
-            drop = true;
-        }
-        if(drop){
-            yy = y+ seconds*0.03f;
-            srcRects = makeRects2();
-            setPosition(x, yy, 1.f, 1.f);
-
-            if(y+seconds > 11.f){
-                drop = false;
-                pang = true;
-                seconds = 0.f;
-            }
-        }
-        if(pang){
-            if(seconds > 1.5f){
-                Scene.top().remove(MainScene.Layer.trap1, this);
-            }
-            setPosition(x, 7.f, 1.f, 1.f);
-            srcRects = makeRects3(0,1,2,3,4, 5,6,7,8);
-        }
-        if(pangEarly){
-            if(seconds > 1.5f){
-                Scene.top().remove(MainScene.Layer.trap1, this);
-            }
-        }
         fixCollisionRect();
+
+        switch(state){
+            case idle:
+                if(seconds>=startTime){
+                    setState(State.ready);
+                    srcRects = makeRects(0,1,2,3,4, 5,6,7,8,9, 10,11);
+                    seconds = 0.f;
+                }
+                break;
+            case ready:
+                if(seconds>=1.1f){
+                    setState(State.drop);
+                }
+                break;
+            case drop:
+                yy = y+ seconds*elapsedSeconds;
+                srcRects = makeRects2();
+                setPosition(x, yy, 1.f, 1.f);
+
+                if(y+seconds > 11.f){
+                    setState(State.pang);
+                    seconds = 0.f;
+                }
+                break;
+            case pang:
+                if(seconds > 1.5f){
+                    Scene.top().remove(MainScene.Layer.trap1, this);
+                }
+                setPosition(x, 7.f, 1.f, 1.f);
+                srcRects = makeRects3(0,1,2,3,4, 5,6,7,8);
+                break;
+            case earlypang:
+                if(seconds > 1.5f){
+                    Scene.top().remove(MainScene.Layer.trap1, this);
+                }
+                break;
+        }
     }
 
     public void pangFunc(float currentY){
-        drop = false;
-        pang = false;
-        pangEarly = true;
+        setState(State.earlypang);
         seconds = 0.f;
         setPosition(x, currentY, 1.f, 1.f);
         srcRects = makeRects3(0,1,2,3,4, 5,6,7,8);
     }
     @Override
     public RectF getCollisionRect() {
-        if(pangEarly || pang)return new RectF(0,0,0,0);
+        if(state==State.earlypang || state == State.pang)return new RectF(0,0,0,0);
 
         return collisionRect;
     }
